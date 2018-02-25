151bbf75578bc474d14baf184cdc8040ed9cbf88
Erdoan Hadzhiyusein <3rdoan@gmail.com>
2018-02-25 16:00:44 +0200

    Add project files.

---
+++ UserLogin/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    class User
+    {
+        public String Username;
+        public String Password;
+        public String FakNum;
+        public Int32 Role;
+
+        public User(String username, String pass, String faknum, Int32 role)
+        {
+            this.Username = username;
+            this.Password = pass;
+            this.FakNum = faknum;
+            this.Role = role;
+        }
+    }
+}
---
+++ UserLogin/UserData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    static class UserData
+    {
+
+        static private User [] _testUsers= new User[3];
+
+        static public User [] TestUsers
+        {
+            get {
+                ResetTestUserData();
+                return _testUsers;
+            }
+            set { }
+        } 
+
+
+        private static void ResetTestUserData()
+        {
+            _testUsers[0] = new User("doandzhi", "123456", "121215079", 1);
+            _testUsers[1] = new User("makaroni", "123456", "121232079", 2);
+            _testUsers[2] = new User("spaghetti", "123456", "121214379", 2);
+        }
+
+        public static User IsUserPassCorrect(String username, String pass)
+        {
+            ResetTestUserData();
+            for (int i=0; i< _testUsers.Length; i++)
+            {
+                if (_testUsers[i].Username.Equals(username) && _testUsers[i].Password.Equals(pass))
+                {
+                    return _testUsers[i];
+                }
+            }
+            return null;
+        }
+    }
+}
